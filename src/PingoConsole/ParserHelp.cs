032c86d68755ebb20c812b6974513541e9fb3b2b
ghstahl@gmail.com <ghstahl@gmail.com>
2015-06-03 09:02:25 -0700

    initial

---
+++ src/PingoConsole/ParserHelp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Fclp.Internals;
+
+namespace PingoConsole
+{
+    static class ParserHelp
+    {
+        public static void HelpDumpOptions(this List<ICommandLineOption> options)
+        {
+            foreach (var option in options)
+            {
+                option.HelpDumpOption();
+                Console.WriteLine();
+            }
+        }
+
+        public static void HelpDumpOption(this ICommandLineOption commandLineOption)
+        {
+            Console.WriteLine(string.Format("Description: {0}", commandLineOption.Description));
+
+            var command = "[";
+            if (commandLineOption.HasShortName)
+            {
+                command += "-" + commandLineOption.ShortName;
+            }
+            if (commandLineOption.HasShortName && commandLineOption.HasLongName)
+            {
+                command += "|";
+            }
+            if (commandLineOption.HasLongName)
+            {
+                command += "--" + commandLineOption.LongName;
+            }
+            command += "]";
+
+            Console.WriteLine(string.Format("    Command:{0}", command));
+        }
+
+        public static void HelpDumpUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("Template: CodeCoverageConverter.exe [-c|--CoverageFile] <path to .coverage file> [-o|--Output] <path to xml file> ");
+            Console.WriteLine("Actual: CodeCoverageConverter.exe --CoverageFile \"h:\\Some\\Path\\abc.coverage\" --Output \"h:\\SomePath\\To\\MyOutput.XML\" ");
+        }
+    }
+}
---
+++ src/PingoConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PingoConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            AppDomain.CurrentDomain.AssemblyResolve +=
+                new ResolveEventHandler(PingoConsole.PingoEmbeddedAssemblies.AssemblyResolver.OnResolveAssembly);
+            MainCore(args);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void MainCore(string[] args)
+        {
+
+            {
+                string[] cultureNames = {"en-US", "fr-FR", "ru-RU", "sv-SE"};
+                var assembly = typeof(PingoConsole.Resources.Common).Assembly;
+
+                var rm = new ResourceManager("PingoConsole.Resources.Common", assembly);
+                foreach (var cultureName in cultureNames)
+                {
+                    CultureInfo culture = CultureInfo.CreateSpecificCulture(cultureName);
+                    Thread.CurrentThread.CurrentCulture = culture;
+                    Thread.CurrentThread.CurrentUICulture = culture;
+
+                    string dateString = rm.GetString("Welcome");
+                    Console.WriteLine("{0} {1:M}.\n", dateString, DateTime.Now);
+                }
+
+
+
+              
+                   // Console.WriteLine(PingoConsole.Resources.Common.Welcome);
+                // your class as shown is Program
+                //  i.e.    CodeCoverageLib.CoverageConverter.LibMain(args);
+            }
+        }
+    }
+}
