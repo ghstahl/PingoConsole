94339d6f142f98bc5baf6b8ede00429915255e97
ghstahl@gmail.com <ghstahl@gmail.com>
2015-07-06 06:32:25 -0700

    missing

---
+++ src/JsonConvert/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Pingo.CommandLine.Composite;
+
+namespace JsonConvert
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(JsonConvert.PingoEmbeddedAssemblies.AssemblyResolver.OnResolveAssembly);
+            MainCore(args);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void MainCore(string[] args)
+        {
+            Pingo.CommandLine.EntryPoint.Program.MefRunnerEntryPoint(new EntryAssemblyEmbeddedMefAssemblies(), args);
+        }
+    }
+}
