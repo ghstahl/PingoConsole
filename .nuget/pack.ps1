a80af121d3ec11727fddc641d997eee0588e5973
ghstahl@gmail.com <ghstahl@gmail.com>
2015-07-07 06:45:09 -0700

    Building nugets for my commands

--- .nuget/pack.ps1
+++ .nuget/pack.ps1
@@ -5,6 +5,8 @@ $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 $version = [System.Reflection.Assembly]::LoadFile("$root\Pingo.CommandLine\bin\Release\Pingo.CommandLine.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
+############################################################################################
+Write-Host "Processing Pingo.Console.nuspec"
 Write-Host "Setting .nuspec version tag to $versionStr"
 
 $content = (Get-Content $root\.nuget\Pingo.Console.nuspec) 
@@ -13,3 +15,37 @@ $content = $content -replace '\$version\$',$versionStr
 $content | Out-File $root\.nuget\Pingo.Console.compiled.nuspec
 
 & $root\.nuget\NuGet.exe pack $root\.nuget\Pingo.Console.compiled.nuspec
+
+############################################################################################
+Write-Host "Processing Pingo.CommandLineHelp.nuspec"
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\.nuget\Pingo.CommandLineHelp.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\.nuget\Pingo.CommandLineHelp.compiled.nuspec
+
+& $root\.nuget\NuGet.exe pack $root\.nuget\Pingo.CommandLineHelp.compiled.nuspec
+
+############################################################################################
+Write-Host "Processing Pingo.JsonConverters.Commands.nuspec"
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\.nuget\Pingo.JsonConverters.Commands.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\.nuget\Pingo.JsonConverters.Commands.compiled.nuspec
+
+& $root\.nuget\NuGet.exe pack $root\.nuget\Pingo.JsonConverters.Commands.compiled.nuspec
+
+############################################################################################
+Write-Host "Processing Pingo.TestCommands.nuspec"
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\.nuget\Pingo.TestCommands.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\.nuget\Pingo.TestCommands.compiled.nuspec
+
+& $root\.nuget\NuGet.exe pack $root\.nuget\Pingo.TestCommands.compiled.nuspec
+
